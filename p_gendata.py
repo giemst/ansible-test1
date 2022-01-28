a2ee25d3a884bf232da0df6411856cd9592b1b71
giemst <pypaz3@gmail.com>
2022-01-28 15:56:46 +0200

    intermediate commit added

--- p_gendata.py
+++ p_gendata.py
@@ -6,10 +6,17 @@ import cx_Oracle
 conn = cx_Oracle.connect("ansible/ansible@192.168.1.232:1521/ORCLPDB1")
 
 c = conn.cursor()
+dcn = 100000     # First commit at that number of rows
+
 for a in range(0, 100000):
     a_num1 =  ''.join(random.choice(string.digits) for i in range(15))
     a_date1 = datetime.date(1920, 1, 1) + datetime.timedelta(days=random.randrange(365*150))
     a_str1 = ''.join(random.choice(string.ascii_letters) for i in range(100))
     c.execute("insert into ansible_test values(:N, :D, :S)" , [a_num1, a_date1, a_str1])
+    
+    if a >= dcn : # check if there is time to commit
+        conn.commit()
+        dcn +=10000   # next commit increament
+
 conn.commit()
 conn.close()
