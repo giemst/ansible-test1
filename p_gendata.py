bd5163df31c40ae8d17490cbafe675448d4134f3
giemst <pypaz3@gmail.com>
2022-01-28 14:54:57 +0200

    number of rows chnaged to 1k

--- p_gendata.py
+++ p_gendata.py
@@ -6,7 +6,7 @@ import cx_Oracle
 conn = cx_Oracle.connect("ansible/ansible@192.168.1.232:1521/ORCLPDB1")
 
 c = conn.cursor()
-for a in range(1, 100):
+for a in range(0, 1000000):
     a_num1 =  ''.join(random.choice(string.digits) for i in range(15))
     a_date1 = datetime.date(1920, 1, 1) + datetime.timedelta(days=random.randrange(365*150))
     a_str1 = ''.join(random.choice(string.ascii_letters) for i in range(100))
