ca2789169a5f21426a44e1ab8a943e98d2d1afaa
giemst <pypaz3@gmail.com>
2022-01-28 15:20:26 +0200

    format added to shrink date value

--- p_getdata.py
+++ p_getdata.py
@@ -8,8 +8,7 @@ with open('p_ansible.csv', 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
 
     c = conn.cursor()
-    c.execute('select id,adate,achar from ansible_test where rownum <101') # use triple quotes if you want to spread your query across multiple lines
+    c.execute("select id,to_char(adate,'yyyy-mm-dd'),achar from ansible_test where rownum <101")
     for row in c:
         writer.writerow(row)
-#    print (row[0], ',', row[1], ',' ,row[2]) # this only shows the first two columns. To add an additional column you'll need to add , '-', row[2], etc.
     conn.close()
